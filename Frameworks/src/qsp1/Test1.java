ccd918bd99485ce3b3e94fc4adf6f2135d6af0fc
HP <HP@DESKTOP-DI5V9S3>
2023-06-01 17:13:30 +0530

    we are pushing the code of BuildB01

---
+++ Frameworks/src/qsp1/Test1.java
@@ -0,0 +1,19 @@
+package qsp1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void data1()
+	{
+		Reporter.log("1",true);
+	}
+	@Test
+	public void data2()
+	{
+		Reporter.log("2",true);
+	}
+
+}
---
+++ Frameworks/src/qsp1/Test2.java
@@ -0,0 +1,14 @@
+package qsp1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	@Test
+	public void data3()
+	{
+		Reporter.log("3",true);
+	}
+
+}
